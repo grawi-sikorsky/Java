9775a4d4d095f3107bbc4897e1ca7f1f613cd893
Printo3DJob <grawires@gmail.com>
2021-05-20 17:20:47 +0200

    a

--- Waluty/waluty/src/main/java/pl/printo3d/waluty/RatesService.java
+++ Waluty/waluty/src/main/java/pl/printo3d/waluty/RatesService.java
@@ -1,37 +1,40 @@
 ﻿package pl.printo3d.waluty;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
 
 import pl.printo3d.waluty.model.RatesModel;
 
 @Service
 public class RatesService {
 
-  RatesModel rm = new RatesModel();
-  RatesModel rates = new RatesModel();
+  RatesModel data = new RatesModel();
 
   public RatesModel getRatesFromAPI()
   {
-    
     StringBuilder builder = new StringBuilder();
-    
-    //RequestHeadersSpec<?> spec = WebClient.create().get().uri("https://openexchangerates.org/api/latest.json?app_id=8011116e5aaa43698b0eb4dc6cb18ba8");
-
+ 
     //builder.append("https://openexchangerates.org/api/latest.json?app_id=8011116e5aaa43698b0eb4dc6cb18ba8");
     builder.append("http://printo3d.pl/latest.json");
     
     RestTemplate restTemplate = new RestTemplate();
-    rates = restTemplate.getForObject(builder.toString(), RatesModel.class);
+    data = restTemplate.getForObject(builder.toString(), RatesModel.class);
 
-    //rm = spec.retrieve().toEntity(RatesModel.class).block().getBody();
 
-    System.out.println(rates);
+    System.out.println(data);
     
-    rates.getRates().entrySet().forEach(e-> System.out.println(e.getKey() + " " + e.getValue()));
+    data.getRates().entrySet().forEach(e-> System.out.println(e.getKey() + " " + e.getValue()));
 
-    return rates;
+    System.out.println("Szukamy KEY: BTC");
+    System.out.println("ZNALEZIONE KUFAAA" + getRateFromCur(data.getRates(), "AUD"));
+
+    return data;
+  }
+
+  public String getRateFromCur(HashMap<String,String> map, String searchKey)
+  {
+    return map.get(searchKey);
   }
 }
