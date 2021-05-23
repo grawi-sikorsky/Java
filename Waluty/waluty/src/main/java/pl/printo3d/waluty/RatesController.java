60a880a77edf40554168cdaaea7d741a5b50bd4f
printo3d <grawires@gmail.com>
2021-05-24 01:30:09 +0200

    exchange gotowe

--- Waluty/waluty/src/main/java/pl/printo3d/waluty/RatesController.java
+++ Waluty/waluty/src/main/java/pl/printo3d/waluty/RatesController.java
@@ -20,12 +20,18 @@ public class RatesController {
   @RequestMapping(value="/", method={RequestMethod.GET})
   public String showRates(Model mdl)
   {
-    ratesService.getRatesFromAPI();
+    //ratesService.getRatesFromAPI();
     //System.out.println(ratesService.getWaluty()); 
 
     mdl.addAttribute("waluty", ratesService.rates.getRates());
-    mdl.addAttribute("basecurrency", ratesService.getBaseCurrency() );
+    mdl.addAttribute("basecurrency", ratesService.getBaseCurrency());
     mdl.addAttribute("currencies", ratesService.getWalutyBasedOnBaseCurrency() );
+    mdl.addAttribute("target", ratesService.findExchangeRateForCode("PLN"));
+    mdl.addAttribute("exchanged", ratesService.getExchangedRate("PLN"));
+    mdl.addAttribute("amountToCalc", "1");
+    mdl.addAttribute("amountCalculated", ratesService.CalculateExchange("1", "USD", "PLN"));
+    mdl.addAttribute("pickedCurrency", "PLN");
+
     //mdl.addAttribute("wynik", ratesService.wynik());
     //mdl.addAttribute("currnames", ratesService.getCurrNames());
 //    ratesService.getCurrNames();
@@ -38,6 +44,7 @@ public class RatesController {
     mdl.addAttribute("waluty", ratesService.findCurrency(szukaj));
     mdl.addAttribute("basecurrency", ratesService.getBaseCurrency());
     mdl.addAttribute("currencies", ratesService.getWalutyBasedOnBaseCurrency() );
+
     //mdl.addAttribute("wynik", ratesService.wynik(swynik));
     System.out.println("REQ1");
     
@@ -54,9 +61,14 @@ public class RatesController {
     mdl.addAttribute("waluty", ratesService.rates.getRates());
     mdl.addAttribute("basecurrency", ratesService.getBaseCurrency());
     mdl.addAttribute("currencies", ratesService.getWalutyBasedOnBaseCurrency() );
+    mdl.addAttribute("target", ratesService.findExchangeRateForCode(tar));
+    mdl.addAttribute("exchanged", ratesService.getExchangedRate(tar));
+
     mdl.addAttribute("amountToCalc", amountToCalc);
     mdl.addAttribute("amountCalculated", ratesService.CalculateExchange(amountToCalc, "USD", tar.toUpperCase()));
     mdl.addAttribute("pickedCurrency", tar.toUpperCase());
+
+
     System.out.println("REQ2");
     
     //System.out.println(ratesService.findCurrency("BTC"));
--- Waluty/waluty/src/main/java/pl/printo3d/waluty/RatesService.java
+++ Waluty/waluty/src/main/java/pl/printo3d/waluty/RatesService.java
@@ -20,28 +20,38 @@ public class RatesService {
   CurNamesModel names = new CurNamesModel();  // Currencies names Model class
   LinkedHashMap<String,String> baseCurrencyValues = new LinkedHashMap<>();  // Converted prices based on picked currency (only USD - fri api..)
 
+  public RatesService()
+  {
+    try
+    {
+      getRatesFromAPI();
+    }
+    catch(Exception e)
+    {
+      System.out.println("Nie udalo sie odczytac danych z API");
+    }
+  }
+
   public void getRatesFromAPI()
   {
     //StringBuilder builder = new StringBuilder();
- 
     //builder.append("https://openexchangerates.org/api/latest.json?app_id=8011116e5aaa43698b0eb4dc6cb18ba8");
     //builder.append("http://printo3d.pl/latest.json");
     
     RestTemplate restTemplate = new RestTemplate();
-    RestTemplate rt = new RestTemplate(); // ??
     rates = restTemplate.getForObject("http://printo3d.pl/latest.json", RatesModel.class);
-    names = rt.getForObject("http://printo3d.pl/currencies.json", CurNamesModel.class);
+    names = restTemplate.getForObject("http://printo3d.pl/currencies.json", CurNamesModel.class);
     
-    //names.getCodenames().entrySet().forEach(e-> System.out.println(e.getKey() + " " + e.getValue()));
-
     //return rates;
   }
 
+  // zwraca exchange rate dla wybranego kodu waluty
   public String getExRateFromName(String searchKey)
   {
     return rates.getRates().get(searchKey);
   }
 
+  // zwraca pare: waluta + exchange rate dla podanego kodu
   public LinkedHashMap<String,String> findCurrency(String search)
   {
     LinkedHashMap <String,String> o = new LinkedHashMap();
@@ -50,22 +60,31 @@ public class RatesService {
     return o;
   }
 
+  public String findExchangeRateForCode(String searchCode)
+  {
+    return rates.getRates().get(searchCode);
+  }
+
+  // zwraca cala mape z danymi pobranycmi z JSONa czyli kursy wymiany z bazowej USD
   public String getWaluty()
   {
     return rates.getRates().entrySet().stream().collect(Collectors.toList()).toString();
   }
 
+  // zwraca same nazwy kodowe walut bez rate ?? [w sumie do wywalenia, mozna chyba zwracac glowna mape i wyswietlac tylko klucz zamiast klucza z wartoscia] - TODO: sprawdzic!
   public String getCurrNames()
   {
     names.getCode().entrySet().forEach(e-> System.out.println(e.getKey() + " " + e.getValue()));
     return names.getCode().entrySet().stream().collect(Collectors.toList()).toString();
   }
 
+  // Zwraca bazowa walute ktorej dotycza wszystkie kursy (z darowego jsona tylko USD..)
   public String getBaseCurrency()
   {
     return rates.getBase();
   }
 
+  // Zwraca mape przeliczonych kursow z domyslnego formatu: [1 'USD' = X 'YYY'] na format: [1 'YYY' = X 'USD'] (1 USD - 0.000043546 BTC  ->  1 BTC = 32000 USD )
   public LinkedHashMap<String,String> getWalutyBasedOnBaseCurrency()
   {
     Set<HashMap.Entry<String, String>> entries = rates.getRates().entrySet();
@@ -85,18 +104,20 @@ public class RatesService {
     return baseCurrencyValues;
   }
 
-  // (AmountToCalculate, baseCurrency, pickedCurrency)
+  public String getExchangedRate(String searchCode)
+  {
+    return baseCurrencyValues.get(searchCode); // aby to nie wywalilo bledu powyzsza funkcja musi byc raz odplaona.
+  }
+
+  // Zwraca przeliczona wymiane po podaniu ilosci, base, i target:
   public String CalculateExchange(String amountToCalculate, String baseCurrency, String pickedCurrency)
   {
-    // poczebujemy:
+    // potrzebujemy:
     /*
         wynik (picked currency) = amount * exRate (picked currency)
     */
-
-    // 1. pociagnac "BTC" picked currency z exchange rates
     String baseRate = rates.getRates().get(baseCurrency.toUpperCase());
     String exRate = rates.getRates().get(pickedCurrency.toUpperCase());
-
 
     BigDecimal wynik = new BigDecimal(amountToCalculate);
     BigDecimal pickedCur = new BigDecimal(exRate);
